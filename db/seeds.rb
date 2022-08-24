76b420905c6c20b3575d95e4643e9518d30fa4ff
Joel Tang <joeltwk@gmail.com>
2022-08-24 19:59:31 +0800

    devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "destroying users..."
+
+User.destroy_all
+
+puts "creating user..."
+
+user = User.create!(
+  email: 'test@email.com',
+  password: 'Password123'
+)
+
+puts "Done"
