8b1e0dfba81351c2994f132d01bc0dc94bbfc8fd
Wan Ting Chua <cawntggreek@outlook.com>
2022-08-24 22:36:30 +0800

    Updating VC-show & edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_08_24_124003) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_124521) do
->>>>>>> 9070c69f677493d2693bbb9493fc5c16c640960c
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,15 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_124521) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.text "comment"
-    t.decimal "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "users_id"
-    t.index ["users_id"], name: "index_reviews_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,5 +39,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_124521) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "reviews", "users", column: "users_id"
 end
