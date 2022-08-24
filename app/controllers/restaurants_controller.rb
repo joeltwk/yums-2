3614ab1b236f7aa78915563cbc05bcbace265291
He Fang Ng <heefang@gmail.com>
2022-08-24 23:19:27 +0800

    Completed CRUD Controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def show
   end
 
   def index
+    @restaurants = Restaurant.all
   end
 
   def new
@@ -18,10 +21,27 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path, status: :see_other
+  end
+
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :cuisine, :description)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
