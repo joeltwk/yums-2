d3fbce89ae9d5e2b79850c01071d38ae4ff2c76d
He Fang Ng <heefang@gmail.com>
2022-08-24 21:50:55 +0800

    new and create controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,24 @@ class RestaurantsController < ApplicationController
 
   def index
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :cuisine, :description)
+  end
+
 end
