a0a1374a0c62bdff514e378db89f3b864c75760d
Samantha Yeang <sammyyeang@gmail.com>
2022-08-25 20:14:04 +0800

    changes in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_124521) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_141911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_124521) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "users_id"
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
     t.index ["users_id"], name: "index_reviews_on_users_id"
   end
 
@@ -48,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_124521) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "restaurants"
   add_foreign_key "reviews", "users", column: "users_id"
 end
