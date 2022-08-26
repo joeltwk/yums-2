0156f97054de015ed475766dd9e9c590b7e1f675
Wan Ting Chua <cawntggreek@outlook.com>
2022-08-26 16:57:34 +0800

    commit for merge

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -9,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_123357) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_124003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
