d720f6a25aba951cf97d71799cf0884461a13083
He Fang Ng <heefang@gmail.com>
2022-08-27 16:08:33 +0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_005627) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_080351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,10 +18,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_005627) do
     t.string "name"
     t.string "address"
     t.string "cuisine"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.boolean "active"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
@@ -46,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_005627) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "role"
-    t.boolean "active"
     t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
