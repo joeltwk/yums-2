ec51c64bcf8a8faa29efbc1f8f5f3f5f6e718095
Samantha Yeang <sammyyeang@gmail.com>
2022-09-07 20:19:20 +0800

    managed to add render to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   before_action :user_login?, only: [:home]
+
   def home
     @restaurants = Collection.all
     @favourite = Favourite.new
-    @restaurant = Restaurant.new
   end
 
   private
@@ -14,5 +14,9 @@ class PagesController < ApplicationController
     else
       redirect_to new_user_session_path
     end
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
